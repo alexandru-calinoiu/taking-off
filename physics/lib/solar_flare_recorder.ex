62ee557c6af7b99b2a7021f3fe32ce601f575525
Calin <calinoiu.alexandru@agilefreaks.com>
2016-07-07 10:28:19 +0300

    first version of the flare recorder

--- physics/lib/solar_flare_recorder.ex
+++ physics/lib/solar_flare_recorder.ex
@@ -2,6 +2,21 @@ defmodule SolarFlareRecorder do
 	use GenServer
 
 	def start_link do
+		Agent.start_link fn -> [] end
+	end
+
+	def record_flare(pid, {:flare} = flare) do
+		Agent.get_and_update pid, fn(flares) ->
+			new_state = List.insert_at flares, -1, flare
+			{:ok, new_state}
+		end
+	end
+end
+
+defmodule SolarFlareRecorderOld do
+	use GenServer
+
+	def start_link do
 		GenServer.start_link __MODULE__, []
 	end
 
